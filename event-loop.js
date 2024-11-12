c7a6253d1df0db8a47be043009fd115d16066199
yukito.takeuchi <iyggf66974@yahoo.ne.jp>
2024-11-12 18:14:57 +0900

    date ok

--- event-loop.js
+++ event-loop.js
@@ -1,6 +1,9 @@
 const fs = require("fs");
+const crypto = require("crypto");
 
 
+const start = Date.now();
+process.env.UV_THREADPOOL_SIZE = 4;
 
 
 setTimeout(() => console.log("Timer 1 finished"), 0);
@@ -15,6 +18,10 @@ fs.readFile("test-file.txt", () => {
   setImmediate(() => console.log("Immediate 2 finished"));
 
   process.nextTick(() => console.log("Process.nextTick"));
+
+
+  crypto.pbkdf2Sync("password", "salt", 100000, 1024, "sha512");
+  console.log(Date.now() - start, "Password encrypted");
 });
 
 
